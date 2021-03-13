674a7f94800bee9407207294a05815bc12e2ff54
robi <robins07@me.com>
2021-03-13 21:17:18 +0100

    :art: Restructure Home Page
    as per the figma, restructure home page with new components: HomeBanner & Faq

--- src/features/home/HomePage.js
+++ src/features/home/HomePage.js
@@ -1,13 +1,12 @@
 import React from 'react';
-
-import Pools from 'features/vault/components/Pools/Pools';
-import Disclaimer from '../../components/Disclaimer/Disclaimer';
+import Faq from '../../components/Faq/Faq';
+import HomeBanner from '../../components/HomeBanner/HomeBanner';
 
 export default function HomePage() {
   return (
     <>
-      <Disclaimer />
-      <Pools fromPage="home" />
+      <HomeBanner />
+      <Faq />
     </>
   );
 }
