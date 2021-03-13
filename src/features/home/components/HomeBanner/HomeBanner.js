2d3858f0317c0d4108c37c55b1a77da60ffd556e
robi <robins07@me.com>
2021-03-14 00:28:49 +0100

    :art: set up home banner sections

--- src/features/home/components/HomeBanner/HomeBanner.js
+++ src/features/home/components/HomeBanner/HomeBanner.js
@@ -5,6 +5,10 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
 import styles from './styles';
+import TotalSupply from '../TotalSupply/TotalSupply';
+import TotalValueLocked from '../TotalValueLocked/TotalValueLocked';
+import Button from 'components/CustomButtons/Button.js';
+import BigNumber from 'bignumber.js';
 
 const useStyles = makeStyles(styles);
 
@@ -14,7 +18,22 @@ const HomeBanner = () => {
 
   return (
     <Grid container item className={classes.root} justify="center">
-      <Typography className={classes.disclaimer}>HOME BANNER GOES HERE</Typography>
+      <TotalValueLocked/>
+      <TotalSupply/>
+      <Button
+        className={`${classes.showDetailButton} ${classes.showDetailButtonContained}`}
+        href="/snowglobes"
+      >
+       Launch App
+      </Button>
+      <Button
+        className={`${classes.showDetailButton} ${classes.showDetailButtonOutlined}`}
+        color="primary"
+        href="https://snowballs.gitbook.io/snowball-finance-docs/"
+        // onClick={() => onDeposit(false)}
+      >
+        Litepaper
+      </Button>
     </Grid>
   );
 };
--- src/features/home/components/HomeBanner/styles.js
+++ src/features/home/components/HomeBanner/styles.js
@@ -1,3 +1,5 @@
+import { primaryColor } from '../../../../assets/jss/material-kit-pro-react';
+
 const styles = theme => ({
   root: {
     flexGrow: 1,
@@ -13,6 +15,32 @@ const styles = theme => ({
     marginBottom: '2rem',
     fontWeight: 900,
     color: theme.palette.text.primary,
+  },
+  showDetailButton: {
+    margin: '12px 5px',
+    fontSize: '14px',
+    fontWeight: 'bold',
+    borderRadius: '5px',
+    width: '160px',
+  },
+  showDetailButtonContained: {
+    backgroundColor: primaryColor[0],
+    '& .MuiButton-label': {
+      color: 'white',
+    },
+  },
+  showDetailButtonOutlined: {
+    backgroundColor: 'transparent',
+    border: `1px solid ${theme.palette.primary.main}`,
+    color: primaryColor[0],
+    '&:hover': {
+      '& .MuiButton-label': {
+        color: 'white',
+      },
+    },
+    '& .MuiTouchRipple-root span': {
+      backgroundColor: primaryColor[0],
+    },
   },
 });
 
