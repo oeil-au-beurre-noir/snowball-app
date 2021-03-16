2a97ed4e81e94af313a81ffc0e4e8532a3172c46
robi <robins07@me.com>
2021-03-16 21:54:29 +0100

    :art: add addresses to configure
    Having main address as an external constants and summary of all basic addresses for the app will facilitate its maintenance

--- src/features/configure/index.js
+++ src/features/configure/index.js
@@ -1,3 +1,5 @@
 export { vaultABI, bnbVaultABI, erc20ABI, strategyABI, multicallBnbShimABI, govPoolABI } from './abi';
 export { pools } from './pools';
 export { staking } from './stake';
+export { iceQueenAddress } from './addresses';
+
