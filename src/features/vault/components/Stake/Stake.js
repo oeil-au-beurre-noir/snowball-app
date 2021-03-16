4fab4fc24d9340973373e10e773ddf86fdefad4f
robi <robins07@me.com>
2021-03-17 00:32:32 +0100

    :art: component structure for staking
    Stake Section only displays if user has anything to stake. I copy/pasted deposit section to modify it later to fit with the act of staking rather than depositing LP.

--- src/features/vault/components/Stake/Stake.js
+++ src/features/vault/components/Stake/Stake.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AccordionDetails from '@material-ui/core/AccordionActions';
 import Grid from '@material-ui/core/Grid';
+import StakeSection from './StakeSection/StakeSection';
+import DepositSection from '../PoolDetails/DepositSection/DepositSection';
 
-const Stake = ({ fromPage}) => {
+
+const Stake = ({ fromPage, pool, balanceSingle, sharesBalance,index}) => {
+
   return (
 
     <AccordionDetails style={{ justifyContent: 'space-between' }}>
       <Grid container>
-        <div>STAKE GOES HERE</div>
+        {sharesBalance > 0 &&
+          <>
+        <div>STAKE BUTTON GOES HERE</div>
+          <StakeSection index={index} pool={pool} balanceSingle={balanceSingle} />
+          </>
+
+        }
+        {sharesBalance == 0 &&
+        <div>YOU NEED TO DEPOSIT IN {pool.name} before you can stake for SNOB</div>
+        }
+
       </Grid>
     </AccordionDetails>
   );
 };
-
+const formatDecimals = number => {
+  return number >= 10 ? number.toFixed(4) : number.isEqualTo(0) ? 0 : number.toFixed(8);
+};
 export default Stake;
